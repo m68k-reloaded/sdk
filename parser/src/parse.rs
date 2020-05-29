076f0f243e8d4ec04590a027f582c9ae5eebe326
Marcel Garus <marcel.garus@gmail.com>
2020-05-29 15:50:27 +0200

    Fix all the compiler errors in the parser

--- parser/src/parse.rs
+++ parser/src/parse.rs
@@ -1,108 +1,17 @@
-use crate::statements::*;
-use m68k_reloaded_common::errors::{Error, ErrorCollector};
-use m68k_reloaded_common::Range;
-use m68k_reloaded_scanner::Token;
-use std::mem;
+// use crate::statements::*;
+// use m68k_reloaded_common::errors::{Error, ErrorCollector};
+// use m68k_reloaded_common::{Cursored, Range};
+// use m68k_reloaded_scanner::Token;
 
-pub fn parse<'e>(tokens: Vec<Token>, errors: &'e mut Collector) -> Parser<'e> {
-    let tokens = tokens
-        .into_iter()
-        .filter(|token| !matches!(token, Token::Whitespace(_)))
-        .collect();
-    Parser {
-        tokens,
-        cursor: 0,
-        errors,
-    }
-}
-
-pub struct Parser<'e> {
-    tokens: Vec<Token>,
-    cursor: usize,
-    errors: &'e mut Collector,
-}
-
-// impl Parser<'_> {
-//     fn is_at_end(&mut self) -> bool {
-//         self.tokens.peek() == None
-//     }
-
-//     fn flush(&mut self) {
-//         self.buffer.clear()
-//     }
-
-//     fn peek(&mut self) -> Option<&Token> {
-//         self.tokens.peek()
-//     }
-
-//     fn advance(&mut self) -> Token {
-//         self.tokens.next().unwrap()
-//     }
-
-//     fn advance_while<Test>(&mut self, test: Test) -> Vec<Token>
-//     where
-//         Test: Fn(&Token) -> bool,
-//     {
-//         while !self.is_at_end() && test(self.peek().unwrap()) {
-//             self.advance();
-//         }
-//         mem::replace(&mut self.buffer, vec![])
-//     }
-
-//     fn range(&self) -> Option<Range> {
-//         if self.buffer.is_empty() {
-//             None
-//         } else {
-//             let start = self.buffer.first().unwrap().range().start;
-//             let end = self.buffer.last().unwrap().range().end;
-//             Some(start..end)
-//         }
-//     }
-
-//     fn scan_next_statement(&mut self) -> Result<Stmt<Statement>, Error> {
-//         let token = match (self.advance(), self.peek()) {
-//             ('(', _) => Ok(Token::OpeningParen(self.range())),
-//             (')', _) => Ok(Token::ClosingParen(self.range())),
-//             (',', _) => Ok(Token::Comma(self.range())),
-//             ('.', _) => Ok(Token::Dot(self.range())),
-//             ('+', _) => Ok(Token::Plus(self.range())),
-//             ('#', _) => Ok(Token::NumberSign(self.range())),
-//             (':', _) => Ok(Token::Colon(self.range())),
-//             ('0'..='9', _) | ('-', '0'..='9') => self.parse_decimal_number(),
-//             ('$', _) => self.parse_hex_number(),
-//             ('-', _) => Ok(Token::Minus(self.range())),
-//             // TODO(marcelgarus): Merge the following branches into one as soon as or-patterns are supported.
-//             (';', _) => self.parse_comment(),
-//             ('*', _) => self.parse_comment(),
-//             // TODO(marcelgarus): Merge the following branches into one as soon as or-patterns are supported.
-//             (' ', _) => Ok(Token::Whitespace(self.range())),
-//             ('\t', _) => Ok(Token::Whitespace(self.range())),
-//             (' ', _) => Ok(Token::Whitespace(self.range())),
-//             ('\r', '\n') => {
-//                 self.advance();
-//                 Ok(Token::Newline(self.range()))
-//             }
-//             ('\n', _) => Ok(Token::Newline(self.range())),
-//             // TODO(marcelgarus): Merge the following branches into one as soon
-//             // as or-patterns are supported.
-//             ('a'..='z', _) => self.parse_identifier(),
-//             ('A'..='Z', _) => self.parse_identifier(),
-//             ('_', _) => self.parse_identifier(),
-//             (current, next) => Err(Error::no_match(self.range(), current, next)),
-//         };
-//         self.flush();
-//         token
-//     }
+// pub fn parse(tokens: Vec<Token>, errors: &mut ErrorCollector) -> Parser {
+//     let tokens = tokens
+//         .into_iter()
+//         .filter(|token| !matches!(token, Token::Whitespace(_)))
+//         .collect();
+//     Parser::from_string(tokens, errors)
 // }
 
-// impl<'t, 'e> Iterator for Parser<'t, 'e> {
-//     // TODO(marcelgarus): implement
-//     type Item = Stmt<Statement>;
-
-//     fn next(&mut self) -> Option<Stmt<Statement>> {
-//         unimplemented!()
-//     }
-// }
+// type Parser<'e> = CursorParser<'e>;
 
 // #[cfg(test)]
 // mod tests {
